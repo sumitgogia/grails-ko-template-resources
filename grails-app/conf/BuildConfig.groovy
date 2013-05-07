e9d4a642ae391ec0712a14941e5ed818e6ffd0f1
sumit <sumit@cloudzilla.com>
2013-05-08 00:51:07 +0530

    Adding missing application.properties. And minor formatting change in BuildConfig.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,24 +3,24 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
 grails.project.dependency.resolution = {
-	// inherit Grails' default dependencies
-	inherits("global") {
-	}
-	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-	repositories {
-		grailsPlugins()
-		grailsHome()
-		grailsCentral()
-	}
-	dependencies {
-	}
+    // inherit Grails' default dependencies
+    inherits("global") {
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+    }
+    dependencies {
+    }
 
-	plugins {
-		runtime ":resources:1.2.RC2"
-		build(":tomcat:$grailsVersion",
-				":release:2.0.3",
-				":rest-client-builder:1.0.2") {
-					export = false
-				}
-	}
+    plugins {
+        runtime ":resources:1.2.RC2"
+        build(":tomcat:$grailsVersion",
+                ":release:2.0.3",
+                ":rest-client-builder:1.0.2") {
+            export = false
+        }
+    }
 }
